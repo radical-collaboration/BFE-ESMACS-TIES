dad23e3e019e595f6224786bacb97a0e6450b85e
darkwhite29 <darkwhite29@gmail.com>
2020-09-25 15:02:11 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+. /ccs/home/litan/miniconda3/etc/profile.d/conda.sh
+conda activate hybridwf
+
+# Feel free to use my credentials, but using your own is recommended
+export RMQ_HOSTNAME=129.114.17.185
+export RMQ_PORT=5672
+export RMQ_USERNAME=litan
+export RMQ_PASSWORD=sccDg7PxE3UjhA5L
+export RADICAL_PILOT_DBURL="mongodb://litan:sccDg7PxE3UjhA5L@129.114.17.185:27017/rct-test"
